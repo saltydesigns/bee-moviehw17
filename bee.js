87457f87ddee546324f19a7b24864d393be9074c
saltydesigns <93166697+saltydesigns@users.noreply.github.com>
2022-12-06 18:28:26 -0800

    I forgot why I changed this

--- bee.js
+++ bee.js
@@ -3,3 +3,8 @@ function fade () {
     $('.preloader').fadeOut("fast", "linear");
     }
     setTimeout(fade, 3000);
+
+    /* mobile nav menu toggle */
+    $(".toggle").on("click", function (){
+        $(".item").toggleClass("active");
+    });
